3fa416c90a7a64c420a0afcda98d00adfb7eaa3d
Hanse Kim <hansekim.dev@gmail.com>
2024-08-28 07:39:45 +0900

    Add spacings

--- .tailwind/spacing.ts
+++ .tailwind/spacing.ts
@@ -2,7 +2,7 @@ import { ThemeConfig } from 'tailwindcss/types/config';
 import { convertToRem, getRange } from './_utils';
 
 export const spacing: ThemeConfig['spacing'] = () => {
-  const sizes = [...getRange(0, 36), 48, 60, 72, 96, 180];
+  const sizes = [...getRange(0, 36), 48, 56, 64, 72, 96, 180];
 
   return Object.fromEntries([
     ...sizes.map((size) => [size, convertToRem(size)]),
