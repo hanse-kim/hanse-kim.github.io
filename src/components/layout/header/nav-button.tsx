67b3bdc2e34b06939c8a353581213ecadf70f9ef
Hanse Kim <hansekim.dev@gmail.com>
2024-08-30 21:34:43 +0900

    Rewrite nav-button

--- src/components/layout/header/nav-button.tsx
+++ src/components/layout/header/nav-button.tsx
@@ -13,12 +13,11 @@ export const NavButton = ({ children, to }: NavButtonProps) => {
 
   return (
     <Link
-      className="cursor-pointer w-fit text-18-300 text-text-footer drop-shadow-transparent transition-shadow disabled:cursor-default desktop:hover:drop-shadow-text desktop:hover:text-text data-[active=true]:drop-shadow-text data-[active=true]:text-text group"
+      className="cursor-pointer w-fit text-18-300 text-text-footer drop-shadow-transparent transition-shadow disabled:cursor-default desktop:hover:drop-shadow-text desktop:hover:text-text data-[active=true]:drop-shadow-text data-[active=true]:text-text after:block after:w-full after:h-[1px] after:bg-text after:scale-x-0 desktop:hover:after:scale-x-100 after:transition-transform after:data-[active=true]:scale-x-100"
       to={to}
       data-active={to === path}
     >
-      <span className="text-text">{children}</span>
-      <div className="w-full h-1 mx-auto transition-transform scale-x-0 bg-text group-desktop:hover:scale-x-100 group-data-[active=true]:scale-x-100" />
+      {children}
     </Link>
   );
 };
