5c15681ccfc0ef3d668673a25cd964bd7534e959
Hanse Kim <hansekim.dev@gmail.com>
2024-09-01 20:13:48 +0900

    Fix markdown styles

--- src/styles/prism-theme.ts
+++ src/styles/prism-theme.ts
@@ -57,9 +57,33 @@ cyan      #2aa198
 green     #859900
 */
 
+pre[class*='language-'] {
+  --base03: #002b36;
+  --base02: #073642;
+  --base01: #586e75;
+  --base00: #657b83;
+  --base0: #839496;
+  --base1: #93a1a1;
+  --base2: #eee8d5;
+  --base3: #fdf6e3;
+  --yellow: #b58900;
+  --orange: #cb4b16;
+  --red: #dc322f;
+  --magenta: #d33682;
+  --violet: #6c71c4;
+  --blue: #268bd2;
+  --cyan: #2aa198;
+  --green: #859900;
+}
+
+.dark pre[class*='language-'] {
+  --base00: #93a1a1;
+  --base1: #657b83;
+}
+
   code[class*='language-'],
   pre[class*='language-'] {
-    color: #657b83; /* base00 */
+    color: var(--base00); /* base00 */
     font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
     font-size: 0.95em;
     text-align: left;
@@ -128,7 +152,7 @@ green     #859900
   .token.prolog,
   .token.doctype,
   .token.cdata {
-    color: #93a1a1; /* base1 */
+    color: var(--base1); /* base1 */
   }
 
   .token.punctuation {
@@ -160,7 +184,7 @@ green     #859900
   }
 
   .token.entity {
-    color: #657b83; /* base00 */
+    color: var(--base00); /* base00 */
     background: #eee8d5; /* base2 */
   }
 
