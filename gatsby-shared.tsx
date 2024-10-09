2a3c279d672d642b94ab29f96adf61e7b6572e4e
Hanse Kim <hansekim.dev@gmail.com>
2024-10-09 23:39:54 +0900

    Fix issue where Partytown only functions on the root page
    - reference: https://github.com/gatsbyjs/gatsby/issues/37024

--- gatsby-shared.tsx
+++ gatsby-shared.tsx
@@ -1,10 +1,18 @@
 import { WrapPageElementBrowserArgs } from 'gatsby';
 import React from 'react';
+import { GoogleAdsense } from 'src/components/ssr/google-adsense';
+import { GoogleAnalytics } from 'src/components/ssr/google-analytics';
 import { Layout } from './src/components/layout';
 
 export const wrapPageElement = ({
   element,
   props,
 }: WrapPageElementBrowserArgs) => {
-  return <Layout pageProps={props}>{element}</Layout>;
+  return (
+    <>
+      <Layout pageProps={props}>{element}</Layout>
+      <GoogleAdsense />
+      <GoogleAnalytics />
+    </>
+  );
 };
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,8 +1,6 @@
 import { RenderBodyArgs } from 'gatsby';
 import { default as React } from 'react';
 import { FontPreload } from 'src/components/ssr/font-preload';
-import { GoogleAdsense } from 'src/components/ssr/google-adsense';
-import { GoogleAnalytics } from 'src/components/ssr/google-analytics';
 import './src/styles/global.css';
 
 export { wrapPageElement } from './gatsby-shared';
@@ -13,8 +11,6 @@ export const onRenderBody = ({
 }: RenderBodyArgs) => {
   setHtmlAttributes({ lang: 'ko' });
   setHeadComponents([
-    ...[FontPreload, GoogleAdsense, GoogleAnalytics].map((Component, index) => (
-      <Component key={index} />
-    )),
+    ...[FontPreload].map((Component, index) => <Component key={index} />),
   ]);
 };
